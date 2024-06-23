8c1ffa466bac1fb6366b4e3f997bbf4ed1880f34
aixion1506 <133094660+aixion1506@users.noreply.github.com>
2024-06-23 20:08:57 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,7 +1,11 @@
-function solution(array, n) {
-    let count = 0;
-    array.forEach(item => {
-        if (item ===n) count++;
-    })    
-    return count;
+// function solution(array, n) {
+//     let count = 0;
+//     array.forEach(item => {
+//         if (item ===n) count++;
+//     })    
+//     return count;
+// }
+
+function solution (array, n) {
+    return array.filter(item => item === n).length;
 }
