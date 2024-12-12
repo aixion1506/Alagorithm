548b28ae6131043e7285bb1e44855eb2434bccb6
aixion1506 <133094660+aixion1506@users.noreply.github.com>
2024-12-12 12:12:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 580 ms, Memory: 117944 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,28 +1,40 @@
 const readline = require("readline");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+let numbers = [];
+let rl;
+let N;
 
-let inputLines = [];
-let N; // 정수의 개수
+function readlineModule() {
+  rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+}
 
-// 첫 번째 줄에서 정수의 개수 N을 읽습니다.
-rl.on("line", (line) => {
-  if (N === undefined) {
-    N = parseInt(line, 10); // 정수의 개수를 정수로 변환
-  } else {
-    inputLines = line.split(" ").map(Number); // N개의 정수를 숫자로 변환
-    const { min, max } = findMinMax(inputLines); // 최솟값과 최댓값 계산
-    console.log(min, max); // 결과 출력
-    rl.close(); // 입력 스트림 닫기
-  }
-});
+function startReadline() {
+  readlineModule();
 
-// 최솟값과 최댓값을 계산하는 함수
+  rl.on("line", (line) => {
+    if (N === undefined) {
+      N = parseInt(line, 10);
+    } else {
+      numbers = line.split(" ").map(Number);
+
+      const { min, max } = findMinMax(numbers);
+
+      console.log(min, max);
+
+      rl.close();
+    }
+  });
+}
+
 function findMinMax(numbers) {
   const min = Math.min(...numbers);
   const max = Math.max(...numbers);
   return { min, max };
 }
+
+startReadline();
+
+module.exports = { findMinMax, startReadline };
