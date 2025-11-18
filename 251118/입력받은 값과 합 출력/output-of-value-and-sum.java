ec58d9a17516cc7db55b9a6c7974f81d92eeda84
aixion1506 <133094660+aixion1506@users.noreply.github.com>
2025-11-18 19:31:29 +0900

    Title: 입력받은 값과 합 출력, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/입력받은 값과 합 출력/output-of-value-and-sum.java
+++ 251118/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -8,6 +8,6 @@ public class Main {
         int A = sc.nextInt();
         int B = sc.nextInt();
         
-        System.out.println(A + " " + B + " " + (A+B));
+        System.out.printf("%d %d %d", A, B, A+B);
     }
 }
