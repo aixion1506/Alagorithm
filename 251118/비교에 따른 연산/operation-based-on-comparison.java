9b0bdc7dd36b205e3a9af291fd63ddd754f08dd4
aixion1506 <133094660+aixion1506@users.noreply.github.com>
2025-11-18 20:41:39 +0900

    Title: 비교에 따른 연산, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/비교에 따른 연산/operation-based-on-comparison.java
+++ 251118/비교에 따른 연산/operation-based-on-comparison.java
@@ -8,11 +8,12 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        if (a>b) {
-            System.out.println(a*b);
-        } else {
-            System.out.println(b/a);
-        }
+        // if (a>b) {
+        //     System.out.println(a*b);
+        // } else {
+        //     System.out.println(b/a);
+        // }
         
+        System.out.println(a>b ? a*b : b/a);
     }
 }
